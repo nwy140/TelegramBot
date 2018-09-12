7ee0af0ad583c8abed47921da15973442cc1bd6c
Nwy <nwy140@gmail.com>
2018-09-12 19:30:53 +0800

    Created http://t.me/CyberzZZ_bot AIBot that responds to happy birthday

--- NodeJS/birthday.js
+++ NodeJS/birthday.js
@@ -1,12 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '625464286:AAGBB-JnnaMd_dl0-jzMhnOEdxRP9X9dF1M';
+const token = '657236041:AAHgRUZtQTxZe6Jt8S453KZZi0FMwBvuCDw';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', msg => {
   var chatId = msg.chat.id;
   var message = msg.text.toString().toLowerCase();
 
-  if (message.include('happy birthday')) {
-    bot.sendMessage(chatId, 'thank you!');
+  if (message.includes('happy birthday')) {
+    bot.sendMessage(chatId, 'thank you! \n A birthday is the anniversary of the birth of a person, or figuratively of an institution. Birthdays of people are celebrated in numerous cultures, often with birthday gifts, birthday cards, a birthday party, or a rite of passage.');
   }
 });
