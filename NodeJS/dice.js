683611282a260d506f71d90c2811d330a76e1230
Nwy <nwy140@gmail.com>
2018-09-12 20:08:18 +0800

    Implemented /Roll

--- NodeJS/dice.js
+++ NodeJS/dice.js
@@ -2,21 +2,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '657236041:AAHgRUZtQTxZe6Jt8S453KZZi0FMwBvuCDw';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', msg => {
-  var chatId = msg.chat.id;
-  var message = msg.text.toString().toLowerCase();
-  var username = msg.chat.first_name;
-  var noOfMsg = msg.message_id;
-
-  var rollnumber = dice(6).toString();
-  if (message.includes('roll')) {
-    bot.sendMessage(chatId, rollnumber);
-  }
-});
 
 
 bot.onText(/\/roll/g, (msg,match)=>{
-
+    var chatId = msg.chat.id;
+    var message = msg.text.toString().toLowerCase();
+    var username = msg.chat.first_name;
+    var noOfMsg = msg.message_id;
+  
+    var rollnumber = dice(6).toString();
+    if (message.includes('roll')) {
+      bot.sendMessage(chatId, rollnumber);
+    }
 });
 function dice(sizes){
     return Math.floor(Math.random() * sizes) + 1;   
